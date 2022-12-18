6cb86d05c7e4ea1839ded8ae44ed8d651ea1df0f
legit0108 <legitgamer2345@gmail.com>
2022-12-19 00:54:26 +0530

    Weekly Contest 324

--- Weekly Contest 324/Smallest Value After Replacing With Sum of Prime Factors.java
+++ Weekly Contest 324/Smallest Value After Replacing With Sum of Prime Factors.java
@@ -1,3 +1,5 @@
+// Solution - 1
+
 // TC : O(num*sqrt(num)), can be optimized using prime sieve
 // SC : O(1)
 
@@ -26,5 +28,59 @@ class Solution {
         if(num>1) sumOfPrimeFactors+=num;
         
         return sumOfPrimeFactors;
+    }
+}
+
+// Solution - 2 : Prime sieve
+
+// TC : O(MAX_SIZE*log(log(MAX_SIZE)))
+// SC : O(MAX_SIZE)
+
+class Solution {
+    private int MAX_SIZE = (int)1e5+1;
+    private static Integer smallestPrimeFactor[];
+    
+    public int smallestValue(int num) {
+        if(smallestPrimeFactor==null) init();
+        
+        while(true){
+            int sumOfPrimeFactors = getSumOfPrimeFactors(num);
+            
+            if(num==sumOfPrimeFactors) break; 
+            else num = sumOfPrimeFactors;
+        }
+        
+        return num;
+    }
+    
+    private int getSumOfPrimeFactors(int num){
+        int sumOfPrimeFactors = 0;
+        
+        while(num>1){
+            int smallestPrime = smallestPrimeFactor[num];
+            
+            while((num%smallestPrime)==0){
+                num/=smallestPrime;
+                sumOfPrimeFactors+=smallestPrime;
+            }
+        }
+
+        return sumOfPrimeFactors;
+    }
+    
+    private void init(){
+        smallestPrimeFactor = new Integer[MAX_SIZE];
+        
+        for(int num=2; num<MAX_SIZE;num++) smallestPrimeFactor[num] = num;
+        
+        for(int prime=2; prime*prime<MAX_SIZE; prime++){
+            if(smallestPrimeFactor[prime]==prime){
+                smallestPrimeFactor[prime] = prime;
+                
+                for(int num=prime*prime; num<MAX_SIZE; num+=prime){
+                    if(smallestPrimeFactor[num]==num) smallestPrimeFactor[num] = prime;
+                }
+            }
+        }
     }
 }
