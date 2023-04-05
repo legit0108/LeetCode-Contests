beff0016ef7aa8a162803cfb184802b567c01dfe
legit0108 <legitgamer2345@gmail.com>
2023-04-06 01:15:46 +0530

    Biweekly Contest 101

--- Biweekly Contest 101/2608. Shortest Cycle in a Graph.java
+++ Biweekly Contest 101/2608. Shortest Cycle in a Graph.java
@@ -1,63 +1,9 @@
-// We can do this by running DFS/BFS from every node and finding the minimum cycle length over all cycles
-// We need to run DFS/BFS from every node because one component can contain multiple cycles
+// We can do this by running BFS from every node and finding the minimum cycle length over all cycles
+// We need to run BFS from every node because one component can contain multiple cycles
 
 // TC: O(V*(V+E))
 // SC: O(V+E)
 // where V = number of vertices, E = number of edges
-
-
-// Solution-1: DFS
-
-class Solution {
-    public int findShortestCycle(int n, int[][] edges) {
-        ArrayList<Integer>[] graph = createGraph(edges, n);
-        int shortestCycle = Integer.MAX_VALUE;
-        
-        for(int node=0; node<n; node++){
-            shortestCycle = Math.min(shortestCycle, dfs(node, -1, 0, graph, new Integer[n]));
-        }
-        
-        if(shortestCycle==Integer.MAX_VALUE) shortestCycle = -1;
-        return shortestCycle;
-    }
-    
-    private ArrayList<Integer>[] createGraph(int[][] edges, int nodes){
-        ArrayList<Integer>[] graph = new ArrayList[nodes];
-        for(int node=0; node<nodes; node++) graph[node] = new ArrayList();
-        
-        for(int[] edge: edges){
-            int node1 = edge[0];
-            int node2 = edge[1];
-            
-            graph[node1].add(node2);
-            graph[node2].add(node1);
-        }
-        
-        return graph;
-    }
-    
-    private int dfs(int node, int parent, int depth, ArrayList<Integer>[] graph, Integer[] dist){
-        dist[node] = depth;
-        int shortestCycle = Integer.MAX_VALUE;
-        ArrayList<Integer> neighbours = graph[node];
-        
-        for(int neighbour: neighbours){
-            if(dist[neighbour]==null){
-                shortestCycle = Math.min(shortestCycle, dfs(neighbour, node, depth+1, graph, dist));
-            }else if(neighbour!=parent){
-                shortestCycle = Math.min(shortestCycle, Math.abs(dist[neighbour]-dist[node])+1);
-            }
-        }
-        
-        return shortestCycle;
-    }
-}
-
-
-// -----X-----X-----X-----X-----X-----X-----X-----X-----X-----X-----X-----X-----
-
-
-// Solution-2: BFS
 
 class Solution {
     public int findShortestCycle(int n, int[][] edges) {
