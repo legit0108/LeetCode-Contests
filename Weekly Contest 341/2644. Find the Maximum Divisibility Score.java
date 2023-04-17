afe1b30b62eac575f240fafbe855ec9bcf6e536a
legit0108 <legitgamer2345@gmail.com>
2023-04-17 12:51:45 +0530

    Weekly Contest 341

--- Weekly Contest 341/2644. Find the Maximum Divisibility Score.java
+++ Weekly Contest 341/2644. Find the Maximum Divisibility Score.java
@@ -25,3 +25,69 @@ class Solution {
         return divWithMaxDivCount;
     }
 }
+
+
+// Optimized solution
+// Find all divisors for all numbers in nums optimally and then find divisor with the maximum divisibility score
+
+class Solution {
+    public int maxDivScore(int[] nums, int[] divisors) {
+       HashMap<Integer, Integer> map = new HashMap(); // count of numbers a divisor divides
+        
+       for(int num: nums){
+           List<Integer> allDivisors = getAllDivisors(num);
+           
+           for(int divisor: allDivisors){
+               map.put(divisor, map.getOrDefault(divisor, 0)+1); 
+           }
+       }
+        
+       int maxDivCount = 0;
+       int divisorWithMaxDivCount = Integer.MAX_VALUE;
+       
+       for(int divisor: divisors){
+           int freq = map.getOrDefault(divisor, 0);
+           
+           if(freq>maxDivCount){
+               maxDivCount = freq;
+               divisorWithMaxDivCount = divisor;
+           }else if(freq==maxDivCount) divisorWithMaxDivCount = Math.min(divisor, divisorWithMaxDivCount);
+       }
+        
+       return divisorWithMaxDivCount;
+    }
+    
+    private List<Integer> getAllDivisors(int num){
+        HashMap<Integer, Integer> primes = new HashMap();
+        
+        for(int div=2; div*div<=num; div++){
+            while(num%div==0){
+                primes.put(div, primes.getOrDefault(div, 0)+1);
+                num/=div;
+            }    
+        }
+        
+        if(num>1) primes.put(num, primes.getOrDefault(num, 0)+1);
+        
+        List<Integer> divisors = new ArrayList();
+        divisors.add(1);
+        
+        for(int prime: primes.keySet()){
+            int freq = primes.get(prime);
+            int curr = 1;
+            int size = divisors.size();
+            
+            while(freq>0){
+                curr = curr*prime;
+                
+                for(int index=0; index<size; index++){
+                    divisors.add(divisors.get(index)*curr);
+                }
+                
+                freq--;
+            }
+        }
+        
+        return divisors;
+    }
+}
